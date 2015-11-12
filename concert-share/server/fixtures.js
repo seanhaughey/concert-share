9d0ec06e8329213d4ee6f73c8551335e8a8e01f5
Sean Haughey <sdhaughey@gmail.com>
2015-11-12 17:06:02 -0600

    added logo and camera png

--- concert-share/server/fixtures.js
+++ concert-share/server/fixtures.js
@@ -1,14 +1,20 @@
 if (Posts.find().count() === 0) {
 	Posts.insert({
-		title: 'Rock On!',
-		image: 'http://www.ks95.com/wp-content/uploads/2014/04/94130125.jpg'
+		image: 'http://www.ks95.com/wp-content/uploads/2014/04/94130125.jpg',
+		author: 'sdhaughey',
+		artist: 'Motley Crue',
+		venue: 'Frank Irwin Center'
 	});
 	Posts.insert({
-		title: 'Laser Show',
-		image: 'http://orig05.deviantart.net/f20c/f/2010/133/f/3/concert_green_by_kristof_clg.jpg'
+		image: 'http://orig05.deviantart.net/f20c/f/2010/133/f/3/concert_green_by_kristof_clg.jpg',
+		author: 'sdhaughey',
+		artist: 'Imagine Dragons',
+		venue: 'Moody Theater'
 	});
 	Posts.insert({
-		title: 'Confetti Cannon',
-		image: 'http://exclaim.ca/images/up-5flaming_lips.jpg'
+		image: 'http://exclaim.ca/images/up-5flaming_lips.jpg',
+		author: 'sdhaughey',
+		artist: 'Flaming Lips',
+		venue: 'Jannus Landing'
 	});
 }

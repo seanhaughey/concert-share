83278921ca9602ed986afd29e8cb418c7df24f43
Sean Haughey <sdhaughey@gmail.com>
2015-11-12 22:50:38 -0600

    comments working

---
+++ concert-share/client/templates/posts/post_page.js
@@ -0,0 +1,4 @@
+Template.postPage.helpers({ comments: function() {
+	return Comments.find({postId: this._id}); 
+	}
+});
--- concert-share/server/fixtures.js
+++ concert-share/server/fixtures.js
@@ -3,18 +3,21 @@ if (Posts.find().count() === 0) {
 		image: 'images/bob_log.jpg',
 		author: 'sdhaughey',
 		artist: 'Bob Log III',
-		venue: 'Mohawk'
+		venue: 'Mohawk',
+		ownerComment: 'Love me some Bob Log III!'
 	});
 	Posts.insert({
 		image: 'images/black_pistol_fire.jpg',
 		author: 'sdhaughey',
 		artist: 'Black Pistol Fire',
-		venue: 'The Scoot Inn'
+		venue: 'The Scoot Inn',
+		ownerComment: 'Black Pistol Fire is awesome!'
 	});
 	Posts.insert({
 		image: 'images/widespread_panic.jpg',
 		author: 'sdhaughey',
 		artist: 'Widespread Panic',
-		venue: 'ACL Live'
+		venue: 'ACL Live',
+		ownerComment: "Amazing light show!"
 	});
 }
